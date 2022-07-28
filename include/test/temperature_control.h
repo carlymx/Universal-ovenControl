9850b9cdb05862d4dc89d276b4e006e652149af3
carlymx <carlymx@gmail.com>
2022-07-28 17:33:56 +0200

    add temperature control dummy+normal

--- include/test/temperature_control.h
+++ include/test/temperature_control.h
@@ -24,7 +24,28 @@ void read_temperature_primary() {
         Serial.println("Primary Sensor (DUMMY): " + String(current_temp));
     }
 }
-
+/*
 void read_temperature_secundary(){
     temp_secondary_sensor = 25;
 }
+*/
+
+// NORMAL MODE
+int read_temperature (byte pin, long resistance){
+    int temp_sensor = analogRead(pin);    // READ TERMISTOR 
+    float res_temp = resistance * (ADC_RATE / (float)temp_sensor); // TENSION TO RESISTENCE 
+    float log_res_temp = log(res_temp);   // LOGARITM FOR EQUATION
+
+    // EQUIATION Steinhart-Hart (º Kelvin):
+    temp_sensor = (1.0 / (c1 + c2*log_res_temp + c3*log_res_temp*log_res_temp*log_res_temp));
+    temp_sensor = temp_sensor - 273.15;   // CELSIUS
+
+    return temp_sensor;
+}
+
+void read_temperature_secundary(){
+    temp_secondary_sensor = read_temperature(SECUNDARY_SENSOR, RESISTANCE_SECUNDARY_SENSOR); 
+    Serial.println("Sensor A2: " + String(temp_secondary_sensor));
+}
+
+
