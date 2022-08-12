1fdf816f3f8ac286546e165af51444c83364e94b
carlymx <carlymx@gmail.com>
2022-08-12 12:12:52 +0200

    lcd1602

--- include/hardware/lcd_custom_characters.h
+++ include/hardware/lcd_custom_characters.h
@@ -11,12 +11,16 @@
 // https://maxpromer.github.io/LCD-Character-Creator/
 //ToDo: CREAR ESTRUCTURAS DE DATOS PARA MOSTRAR EN PANTALLA
 
+// [ ] * * 2 0 0 / 2 0 0 * * * * *
+// [ ] * * * * * * * * * * * * * *
 
 // LCD 1602 (COLUM, FILE)
 byte char_resis_a = lcd.setCursor(0,0);
 byte char_resis_b = lcd.setCursor(1,0);
 byte char_resis_c = lcd.setCursor(0,1);
 byte char_resis_d = lcd.setCursor(1,1);
+
+byte char_temp_sensor = lcd.setCursor(,0);
 
 struct data_for_lcd_1602{
 
