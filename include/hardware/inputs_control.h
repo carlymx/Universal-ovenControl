5c31d5f33524851217e5e0c7942ba798a86e23df
Carlos Hernández <carlymx@gmail.com>
2022-08-19 16:45:30 +0200

    ...

--- include/hardware/inputs_control.h
+++ include/hardware/inputs_control.h
@@ -25,30 +25,25 @@ void read_inputs() {
     }
 }
 
-
-//----------- GLOBAL INPUTS VARIBLES: ---------
-byte analog_bottons_list[4] = {ANALOG_BUTTON1, ANALOG_BUTTON2, ANALOG_BUTTON3, ANALOG_BUTTON4};
-//int adc_values_array[8] = {1024, 512, 1024, 512, 1024, 512, 1024, 512};
-//float adc_values_array[] = {0,0,0,0,0,0,0,0};
-bool botton_active = false;
-
-
 void read_analog_inputs(){
-    byte input_push = 0;
     for(byte i=0; i<sizeof(analog_bottons_list); i++){
         int var_dr = digitalRead(analog_bottons_list[i]);
         if (var_dr >= (ADC_VALUE_B01-DELTA_INPUT) && botton_active == false){
             botton_active = true;
-            input_push = 1;
+            input_push = 1+i;
             
         }
         else if(var_dr >= (ADC_VALUE_B02-DELTA_INPUT) && botton_active == false){
             botton_active = true;
-            input_push = 2;
+            input_push = 2+i;
             
         }
         else if(var_dr < (ADC_VALUE_B02-DELTA_INPUT) && botton_active == true){
             botton_active = false;
+            input_push = 0;
         }
+    }
+    if(input_push != 0){
+        Serial.println(input_push);
     }
 }
