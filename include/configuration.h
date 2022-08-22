ef1f899df9e6fe2e220a873c53d4ececb3c99361
carlymx <carlymx@gmail.com>
2022-08-22 12:39:54 +0200

    ...

--- include/configuration.h
+++ include/configuration.h
@@ -6,3 +6,11 @@
     jordi@surfzone.org, carlymx@gmail.com
     2022
 ***************************************************************/
+
+#if defined LGT8F328P           // FOR LGT8F328P (ARDUINO COMPATIBLE)
+    #include <boards/LGT8F328P/hardware_conf.h>
+#elif defined RASPBERRYPI_PICO  // FOR RASPBERRY PI PICO /W
+    #include <boards/rasberryPICO/hardware_conf.h>
+#elif defined ARDUINO_BOARDS    // FOR ARDUINO AVR BOARDS (UNO, NANO, MINI...)
+    #include <boards/arduino/hardware_conf.h>
+#endif
