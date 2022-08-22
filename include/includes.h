814fb7bb9c39eede073eab55bc431c2edb04e327
carlymx <carlymx@gmail.com>
2022-08-22 12:11:40 +0200

    add structure dir

--- include/includes.h
+++ include/includes.h
@@ -1,8 +1,8 @@
 /***************************************************************  
     openELECTRO
-    Home appliance control, based on arduino and other MPU
+    Home appliance oven control, based on arduino and other MPU
 
-    https://github.com/carlymx/openELECTRO
+    https://github.com/carlymx/Universal-ovenControl
     jordi@surfzone.org, carlymx@gmail.com
     2022
 ***************************************************************/
